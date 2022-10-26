153524ca66b211edcf094bc562b796bcc565ad43
Vitoria Regina <116230301+viregina@users.noreply.github.com>
2022-10-26 23:14:26 +0000

    comando input

---
+++ aula1.py
@@ -0,0 +1,29 @@
+# Meu primeiro projeto Python!!!
+#
+# print() = comando de saida
+print("Alo Mundo!")
+
+# Quando quiser guarda uma String! (frase)
+nome = "Vitoria"
+#Quando quiser guarda numero inteiro
+idade = 22
+
+#Exibir o meu nome (que esta dentro da variavel nome)
+print(nome)
+
+# Quando quiser exibir a frase "minha idade é" completando com o conteúdo da variavel idade
+#print (meu nome é)
+print("minha idade é "+str(idade)+ " anos ")
+print(f"minha idade é {idade} anos" )
+print("minha idade é {} anos".format(idade))
+
+
+
+
+
+
+
+'''
+comentario em bloco
+
+'''
--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-# Meu primeiro projeto Python!!!
-#
-# print() = comando de saida
-print("Alo Mundo!")
+# comando input(): quero permitir que 
+# o usuário digite algo...
+nome = input("Digite seu nome: ")
+#pede a idade para o usario "Qual sua idade
+idade = int(input("Digite sua idade: "))
 
-# Quando quiser guarda uma String! (frase)
-nome = "Vitoria"
-#Quando quiser guarda numero inteiro
-idade = 22
+#comando de saída..exibir na tela
+print(f"Boa noite, seu nome é {nome}")
+#exiba "Sua idade é..."
+print("Sua idadee é {}".format(idade))
 
+#e se eu quisesse mostrar o DOBRO da idade informada?
+dobro = idade * 2
+print("O dobro da idade informada é {}".format(dobro))
 
-#Exibir o meu nome (que esta dentro da variavel nome)
-print(nome)
-
-
-
-
-'''
-comentario em bloco
-
-'''
