e2abb6fc915ee41c6d8625f3c728a0d05013ad8c
Vitoria Regina <116230301+viregina@users.noreply.github.com>
2022-10-27 00:36:46 +0000

    condicionais

--- aula2-2022-10-26.py
+++ aula2-2022-10-26.py
@@ -1,13 +1,14 @@
-# comando input(): quero permitir que 
+
+# comando input(): quero permitir que
 # o usuário digite algo...
 nome = input("Digite seu nome: ")
-#pede a idade para o usario "Qual sua idade
+#pede a idade para o usuário "Qual sua idade?"
 idade = int(input("Digite sua idade: "))
 
 #comando de saída..exibir na tela
 print(f"Boa noite, seu nome é {nome}")
-#exiba "Sua idade é..."
-print("Sua idadee é {}".format(idade))
+#exiba "Sua idade é ..."
+print("Sua idade é {}".format(idade))
 
 #e se eu quisesse mostrar o DOBRO da idade informada?
 dobro = idade * 2
@@ -17,5 +18,13 @@ print("O dobro da idade informada é {}".format(dobro))
 #Se a pessoa for maior de idade, mostre "Você é maior de idade, ótimo! Já pode beber ou dirigir"
 if idade >= 18:
   print("Você é maior de idade, ótimo! Já pode beber ou dirigir")
+else:
+  print("Você é xóven ainda, xóven ainda...")
+
+#E se eu quisessem perguntar o gênero (M = Masculino e F = Feminino)
+#Mostre (...e você também precisa/precisou prestar o serviço militar obrigatório)
 
 
+
+
+print("vai ser executada de qualquer jeito")
